848d1e66ca12d82003e44d6165dffd7567281ee4
Alexander-Bankov <1bankov20032mail.ru>
2025-03-15 19:27:57 +0400

    правки по 3 лабе сдано

--- src/main/java/com/example/labs/Config/SecurityConfig.java
+++ src/main/java/com/example/labs/Config/SecurityConfig.java
@@ -27,38 +27,6 @@ public class SecurityConfig {
     @Value("${security.roles}")
     private List<String> role;
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-////        http
-////                .cors(cors->cors.disable())
-////                .authorizeHttpRequests((requests) -> requests
-////                        .requestMatchers("/", "/home", "/login").permitAll()
-////                        .requestMatchers(HttpMethod.POST, "/api/project/**").hasRole(role.get(1))
-////                        .requestMatchers(HttpMethod.PUT, "/api/project/**").hasRole(role.get(1))
-////                        .requestMatchers(HttpMethod.DELETE, "/api/project/**").hasRole(role.get(1))
-////                        .requestMatchers("/swagger-ui.html").hasRole(role.get(1))
-////                        .anyRequest().authenticated()
-////                ) .formLogin((form) -> form
-////                        .loginPage("/login.html")
-////                        .loginProcessingUrl("/login")
-////                        .defaultSuccessUrl("/api/project?search")
-////                        .failureUrl("/login.html?error=true")
-////                        .permitAll()
-////                )
-////                .logout(logout -> logout.permitAll());
-////        return http.build();
-//        http
-//                .csrf((csrf) -> {csrf.disable();})
-//                .authorizeHttpRequests(
-//                        (requests) -> requests
-//                                .requestMatchers(HttpMethod.POST, "/projects").hasRole("ADMIN")
-//                                .requestMatchers(HttpMethod.PUT, "/projects/{id}").hasRole("ADMIN")
-//                                .requestMatchers(HttpMethod.DELETE, "/projects/{id}").hasRole("ADMIN")
-//                                .anyRequest().authenticated()
-//                ).httpBasic(Customizer.withDefaults());
-//        return http.build();
-//    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
